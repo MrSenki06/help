6ce28c1293c9f80863a9b964d38a4d8a7e826a1f
MrSenki06 <93594938+MrSenki06@users.noreply.github.com>
2022-10-10 20:44:39 +0200

    Create first.py

---
+++ first.py
@@ -0,0 +1,10 @@
+#komment
+
+x=8#váltózó érték adása
+z=input("szöveg")#szöveg bekérése
+y=int(input("szam"))#szám bekerése
+
+import math #math impotálása
+powint=int(input("hányadikra emeli"))
+y=math.pow(y,powint)#az y ** powint
+print(y)#kiírás
